5cada7d1ce622d4d6e41ae8b2835aa677d9093da
Jef Mayer <jef.a.mayer@gmail.com>
2023-08-15 11:58:49 -0700

    adding document.js, google fonts include

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Muli:wght@200;500;900&family=Rubik+One&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
